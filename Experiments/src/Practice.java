4f6a0bf9a405d6bc72b4788ac0fcf1a4401923e4
DestineeHMonroe <Destinee.Monroe@outlook.com>
2014-11-04 01:38:18 -0500

    Using a String and an Array
    Learning how to manipulate a list of numbers.

---
+++ Experiments/src/Practice.java
@@ -0,0 +1,21 @@
+
+public class Practice {
+
+	public static void main(String[] args){
+		for(int i=1; i<=36; i++){
+			if (i%6==0){
+				System.out.println("ButterFly");
+			}
+			else if(i%2==0){
+				System.out.println("Butter");
+			}
+			else if(i%3==0){
+				System.out.println("Fly");
+			}
+			else{
+				System.out.println(i);
+			}
+				
+	}
+}
+}
---
+++ Experiments/src/Sample.java
@@ -0,0 +1,19 @@
+/* This is a code that uses an Array.
+ * I want to come back to this code to 
+ * come up with a way to list integers 1-20
+ * without listing them manually.
+ */
+
+
+public class Sample {
+	public static void main(String[] args){
+		
+		//int[] one = new int[5]; // same as {0,0,0,0}
+		//int[] one= new int[100]; // 0-99 all zeroes
+		//list[20]= ???
+		
+		int[] list={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+		
+		System.out.println(list[5]);
+	}
+}
